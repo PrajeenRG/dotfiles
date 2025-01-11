dc1205424e0e9d9ecc8e673f4be32ac8a8a77a7a
R G Prajeen <p@prajeen.com>
2025-01-11 23:15:12 +0530

    Add gnupg setup using yubikey

--- .config/zsh/conf.d/gpg.zsh
+++ .config/zsh/conf.d/gpg.zsh
@@ -5,3 +5,6 @@
 export GNUPGHOME=${XDG_DATA_HOME:=~/.local/share}/gnupg
 [[ -e $GNUPGHOME:h ]] || mkdir -p $GNUPGHOME:h
 alias gpg="command gpg --homedir \"\$GNUPGHOME\""
+
+# setup gpg-agent as ssh-agent
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
